7ebc93d88cf9f8da0e447b297320ec3ef39e251b
K. Hamilton <kh5u24@soton.ac.uk>
2025-04-29 22:21:51 +0100

    debugged alu_stim.sv

--- testbench/alu_stim.sv
+++ testbench/alu_stim.sv
@@ -5,12 +5,15 @@ module alu_stim;
 
   logic alu_ctrl;
 
-  logic [7:0] input1;
-  logic [7:0] input2;
+  logic signed [7:0] input1;
+  logic signed [7:0] input2;
+  logic signed [15:0] mult;
+  logic signed [7:0] add;
 
-  wire [7:0] result;
+  wire signed [7:0] result;
 
   logic [31:0] random;
+  logic signed [7:0] tb_result;
 
   alu aludut (
       .alu_ctrl(alu_ctrl),
@@ -20,14 +23,31 @@ module alu_stim;
   );
 
   initial begin
+    alu_ctrl = 1;
+
+    for (int i = 0; i < 1000; i++) begin
+      random = $urandom(i);
+      input1 = random[7:0];
+      input2 = random[15:8];
+      tb_result = input1 + input2;
+      #50
+      if (result != tb_result) $display("fail");
+      #50;
+    end
+
     alu_ctrl = 0;
 
     for (int i = 0; i < 1000; i++) begin
       random = $urandom(i);
       input1 = random[7:0];
       input2 = random[15:8];
-      if (result != input1 + input2) $Display("fail");
+      mult = input1 * input2;
+      tb_result = mult[14:7];
+      #50;
+      if (result != tb_result) $display("fail: %d", i);
+      #50;
     end
+    $finish;
   end
 endmodule
 
